5633bec0c591e4b3f3fbc894c813c31bd14748dc
parsadnsh <parsa98dnsh@gmail.com>
2022-02-26 23:03:06 +0330

    primary classes

---
+++ src/org/bihe/beans/Customer.java
@@ -0,0 +1,80 @@
+package org.bihe.beans;
+
+import java.util.Date;
+
+public class Customer {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+    private Date registrationDate;
+    private String phoneNo;
+
+    public Customer(int id, String firstName, String lastName, String email, int age, Date registrationDate, String phoneNo) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.registrationDate = registrationDate;
+        this.phoneNo = phoneNo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
